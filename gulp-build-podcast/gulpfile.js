a5c9853292a6d9c31be6ca07e2ea842bcc53dfa5
Yulia <yulia.scherbina@gmail.com>
2017-11-17 13:23:41 +0100

    livereload with gulp

--- gulp-build-podcast/gulpfile.js
+++ gulp-build-podcast/gulpfile.js
@@ -1,59 +1,40 @@
-// Load Node modules/plugins
-var gulp = require('gulp'),
-    minifyCSS = require('gulp-minify-css'),
-    concat = require('gulp-concat'),
-    myth = require('gulp-myth'),
-    sass = require('gulp-sass'),
-    uglify = require('gulp-uglify'), // minify code to reduce the file size of the concatenated js file
-    del = require('del'),
-    rename = require('gulp-rename'),
-    //jshint = require('gulp-jshint'), // analyze js files and report any errors that could potentially break an application
-    plumber = require('gulp-plumber'),
-    imagemin = require('gulp-imagemin'), // optimize images by minifying them, which help reduce the load times of the project
-    connect = require('connect'),
-    serve = require('serve-static'),
-    browsersync = require('browser-sync'), // handle browser refreshing
-    beeper = require('beeper'),
-    sourcemaps = require('gulp-sourcemaps');
+'use strict';
 
-// Writing a task
-// gulp.task(name, function() {
-//   return gulp
-//       .src(path) - input
-//       .pipe(plugin)
-//       .pipe(plugin)
-//       .pipe(gulp.dest(path)) - output;
-// });
+const gulp = require('gulp'),
+      minifyCSS = require('gulp-minify-css'),
+      concat = require('gulp-concat'),
 
-gulp.task('delete', function() {
-  del(['assets/*']), function(err) {
-    console.log('Files deleted');
-  }
-});
+      rubyScss = require('gulp-ruby-sass'),
+      scss = require('gulp-sass'),
+
+      uglify = require('gulp-uglify'), // minify code to reduce the file size of the concatenated js file
+      del = require('del'),
+      rename = require('gulp-rename'),
+
+      jshint = require('gulp-jshint'), // analyze js files and report any errors that could potentially break an application
+
+      plumber = require('gulp-plumber'), // handle errors
+      imagemin = require('gulp-imagemin'), // optimize images by minifying them, which help reduce the load times of the project
+
+      connect = require('connect'),
+      serve = require('serve-static'),
+      browsersync = require('browser-sync'), // handle browser refreshing
+      livereload = require('gulp-livereload'),
+      beeper = require('beeper'),
+      sourcemaps = require('gulp-sourcemaps');
+
 
 // clean task
 gulp.task('clean', function(cb) {
   return del(['assets'], cb);
-})
+});
 
 // error handler
-function onError(err) {
+function errorLog(error) {
   beeper();
-  console.log(err)
+  console.error.bind(error);
+  this.emit('end');
 }
-
-// Process styles
-gulp.task('styles', function() {
-  return gulp.src('css/*.css')
-      .pipe(plumber({
-              errorHandler: onError
-      }))
-      .pipe(concat('styles.css'))
-      //.pipe(myth)
-      .pipe(minifyCSS())
-      .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('assets/css'));
-});
 
 // writing the Images task
 gulp.task('images', function() {
@@ -62,11 +43,26 @@ gulp.task('images', function() {
          .pipe(gulp.dest('assets/images'));
 });
 
+// Process styles
+gulp.task('styles', function() {
+  return scss('scss/*.scss', {
+      style: 'compressed',
+      loadPath: ['scss/*.scss']})
+      .on('error', errorLog)
+      .pipe(concat('styles.css'))
+      .pipe(minifyCSS())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('assets/css'))
+      .pipe(livereload());
+});
+
 // Process scripts
 gulp.task('scripts', function() {
   return gulp.src('js/*.js')
       .pipe(sourcemaps.init())
-      .pipe(plumber())
+      .pipe(plumber({
+          errorHandler: errorLog
+      }))
       .pipe(concat('all.js'))
       //.pipe(jshint.reporter('default'))
       .pipe(uglify())
@@ -93,23 +89,28 @@ gulp.task('browsersync', function(cb) {
   }, cb);
 });
 
-// the watch task / Watch files for changes
-// gulp.task('watch', function() {
-//   gulp.watch('css/*.css', ['styles']);
-//   gulp.watch('js/*.js', ['scripts']);
-//   gulp.watch('images/*', ['images']);
-// });
+// Watch files for changes
 gulp.task('watch', function() {
-  gulp.watch('css/*.css',
-      gulp.series('clean', 'styles', browsersync.reload));
-  gulp.watch('js/*.js',
-      gulp.series('scripts', browsersync.reload));
-  gulp.watch('images/*',
-      gulp.series('images', browsersync.reload));
+
+  livereload.listen();
+
+  gulp.watch('/scss/*.scss', ['styles']);
+  gulp.watch('js/*.js', ['scripts']);
+  gulp.watch('images/*', ['images']);
 });
 
+
+// gulp.task('watch', function() {
+//   gulp.watch('scss/*.scss',
+//       gulp.series('clean', 'styles', browsersync.reload));
+//   gulp.watch('js/*.js',
+//       gulp.series('scripts', browsersync.reload));
+//   gulp.watch('images/*',
+//       gulp.series('images', browsersync.reload));
+// });
+
 // Default task
-gulp.task('default', ['delete', 'styles', 'scripts', 'images', 'browsersync', 'watch']);
+gulp.task('default', ['clean', 'styles', 'scripts', 'images', 'browsersync', 'watch']);
 
 // gulp.task('default',
 //     gulp.parallel['delete', 'styles', 'scripts', 'images', 'browsersync', 'server', 'watch']);
